1717bca8e7179b6b9afc3f929aa03fb8e71a79a0
bunnyrabbit0010 <bunnyrabbit.aws@gmail.com>
2024-08-01 11:34:49 -0700

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -6,8 +6,8 @@ class MyLogger:
         self.logger.setLevel(logging.DEBUG)
 
         # Create a file handler
-        file_handler = logging.FileHandler('my_app.log')
-        file_handler.setLevel(logging.INFO)
+        #file_handler = logging.FileHandler('my_app.log')
+        #file_handler.setLevel(logging.INFO)
 
         # Create a console handler
         console_handler = logging.StreamHandler()
@@ -15,11 +15,11 @@ class MyLogger:
 
         # Create a formatter
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        file_handler.setFormatter(formatter)
+        #file_handler.setFormatter(formatter)
         console_handler.setFormatter(formatter)
 
         # Add handlers to the logger
-        self.logger.addHandler(file_handler)
+        #self.logger.addHandler(file_handler)
         self.logger.addHandler(console_handler)
 
     def logDebug(self, message):
